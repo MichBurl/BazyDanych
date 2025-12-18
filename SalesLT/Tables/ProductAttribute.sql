e4c027eb6c9d1c1ad51e0ae44958b5063b42b0ae
MichBurl <michal.burliga03@gmail.com>
2025-12-18 15:39:53 +0100

    lab-3

---
+++ SalesLT/Tables/ProductAttribute.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [SalesLT].[ProductAttribute] (
+    [AttrID]    INT                                         IDENTITY (1, 1) NOT NULL,
+    [ProductID] INT                                         NOT NULL,
+    [Data]      XML(CONTENT [SalesLT].[ProductAttrsSchema]) NOT NULL,
+    PRIMARY KEY CLUSTERED ([AttrID] ASC),
+    FOREIGN KEY ([ProductID]) REFERENCES [SalesLT].[Product] ([ProductID])
+);
+
---
+++ SalesLT/XMLSchemaCollections/ProductAttrsSchema.sql
@@ -0,0 +1,23 @@
+﻿CREATE XML SCHEMA COLLECTION [SalesLT].[ProductAttrsSchema]
+    AS N'<xsd:schema xmlns:xsd="http://www.w3.org/2001/XMLSchema">
+  <xsd:element name="Attributes">
+    <xsd:complexType>
+      <xsd:complexContent>
+        <xsd:restriction base="xsd:anyType">
+          <xsd:sequence>
+            <xsd:element name="Item" minOccurs="5" maxOccurs="unbounded">
+              <xsd:complexType>
+                <xsd:simpleContent>
+                  <xsd:extension base="xsd:string">
+                    <xsd:attribute name="Name" type="xsd:string" use="required" />
+                  </xsd:extension>
+                </xsd:simpleContent>
+              </xsd:complexType>
+            </xsd:element>
+          </xsd:sequence>
+        </xsd:restriction>
+      </xsd:complexContent>
+    </xsd:complexType>
+  </xsd:element>
+</xsd:schema>';
+
